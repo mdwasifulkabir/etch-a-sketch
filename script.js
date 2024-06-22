3acb94f79bc786933e21f4c987560f59851507d1
mdwasifulkabir <wasifulkabir9016@gmail.com>
2024-06-22 13:11:36 +0800

    Make divs square and occupy the entire viewport

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const container = document.querySelector("#container");
 
 for (let i = 0; i < 16; i++) {
   const row = document.createElement("div");
+	row.classList.add("row");
+	
   for (let j = 0; j < 16; j++) {
     const col = document.createElement("div");
+		col.classList.add("col");
 		col.textContent = j;
 		row.appendChild(col);
   }
